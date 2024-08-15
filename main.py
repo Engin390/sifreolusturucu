c81440ba5b0e352652f5f17e55f29af7cfb4c433
Engin390 <enginirmak390@gmail.com>
2024-08-15 22:09:08 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+sayi = int(input("Şifreniz kaç karakterden oluşsun?"))
+
+
+password = ""
+for i in range(sayi):
+    password += random.choice(karakterler)
+
+
+print("Şifreniz:", password)
+
+while True:
+    if sayi==0:
+        print("bu parola çok uzun, siteler kabul etmeyebilir")
